54336c6b5ec113bf9c6714f54c736bdb1b4f35c1
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-02-16 02:03:29 +0800

    feat: Added the function to be able to publish messages to google pubsub
    See also: #5

--- R/topics.R
+++ R/topics.R
@@ -12,7 +12,32 @@ list_topics <- function(project, pageSize, pageToken){
   # Get service account credentials
   token <- get_token()
   config <- httr::config(token=token)
-  result <- httr::GET(url, config = config, accept_json())
+  result <- httr::GET(url, config = config, accept_json(), encode = 'json')
   result_content <- content(result)
   return(result_content)
 }
+
+
+#' Publish a message to the current Google Pubsub project
+#' @param project Name of Google Cloud Project
+#' @param topic Name of the topic to publish to
+#' @param data The message payload. You do not need to encode the message. The function internally will encode
+#' your message
+#' @param attribute_list Optional attributes for this message. Only accepts a named list
+#' @importFrom httr POST config accept_json content
+#' @importFrom RCurl base64
+#' @export
+publish_message <- function(project, topic, data, attribute_list){
+  # Get URL endpoint
+  url <- get_endpoint('pubsub.topics.publish', project, topic = topic)
+  # Get service account credentials
+  token <- get_token()
+  config <- httr::config(token=token)
+  # Prepare the post params
+  pubsub_msg <- list(data=as.character(base64(data)), attributes=attribute_list)
+  params <- list(messages=list(pubsub_msg))
+  result <- httr::POST(url, config = config, accept_json(), body = params, encode = 'json')
+  result_content <- content(result)
+  return(result_content)
+}
+

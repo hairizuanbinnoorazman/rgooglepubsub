21a6269ded39c553e90be26bbb4b75a6a9c7b59b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-02-16 00:14:24 +0800

    feat: Added a function to allow users to search for the package name in order to be able to understand all the functions available in the package

---
+++ R/rgooglepubsub.R
@@ -0,0 +1,7 @@
+#' rgooglepubsub: Giving R users access to server utilities
+#' @description This package aims is to give R users the capability to have full programmatic
+#' access to Google Pubsub tool
+#'
+#' @docType package
+#' @name rgooglepubsub
+NULL
